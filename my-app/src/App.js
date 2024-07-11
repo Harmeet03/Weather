0f967e9bafdc7912bb1a1f0625d5a50fe971c662
Harmeet <102729017+Harmeet03@users.noreply.github.com>
2024-07-12 01:10:02 +0530

    React Final Changes Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,10 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [none, setNone] = useState(false);
+  let s_error = document.getElementById('search_error');
+  let srv_error = document.getElementById('server_error');
 
   const FetchData = async (e) => {
     e.preventDefault();
@@ -13,18 +17,31 @@ function App() {
 
       if(response.ok){
         console.log(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ca2c79f29c31549b8e4e93c97cf0a293&units=metric`);
+        setLoading(false);
         const data = await response.json();
         setWeather(data);
+        setNone(false);
       }
       else{
         console.log('Error in fetching data');
+        setLoading(false)
+        s_error.style.display = 'block';
+        srv_error.style.display = 'none';
+        setNone(true);
       }
     }
     catch(error){
       console.log('Server error: ', error);
+      setLoading(false);
+      s_error.style.display = 'none';
+      srv_error.style.display = 'block';
+      setNone(true);
     }
   }
 
+  if(!weather){
+    return setLoading(true);
+  }
 
   return (
     <>
@@ -38,44 +55,58 @@ function App() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Helmet>
-      <div className='content'>
-        <form className='search' onSubmit={FetchData}>
-          <input name='search' type='text' placeholder='Enter city' required onChange={(e) => setCity(e.target.value)}></input>
-          <button type='submit'><i className='fa fa-search'></i></button>
-        </form>
-        
-        <div className='place'>
-          <h1> {weather.name}, {weather.sys?.country} </h1>
-        </div>
-        
-        <div className='temp_detail'>
-          <div className='left'>
-            <img src={`https://openweathermap.org/img/wn/${weather.weather && weather.weather[0].icon}@2x.png`} alt='not available'></img>
-            <p> {weather.weather && weather.weather[0].main} </p>
-          </div>
-          <div className='right'>
-            <div className='temp'>
-              <h3> Temperature </h3>
-              <p> {weather.main?.temp} °C </p>
-            </div>
-            <div className='extra'>
-              <div className='wind'>
-                <h3> Wind </h3>
-                <p> {weather.wind?.speed} KM/H </p>
-              </div>
-              <div className='humidity'>
-                <h3> Humidity </h3>
-                <p> {weather.main?.humidity}% </p>
-              </div>
-              <div className='feels'>
-                <h3> Feels Like </h3>
-                <p> {weather.main?.feels_like}°C </p>
-              </div>
-            </div>
-          </div>
 
-        </div>
-      </div>
+      <form className='search' onSubmit={FetchData}>
+        <input name='search' type='text' placeholder='Enter city' required onChange={(e) => setCity(e.target.value)}></input>
+        <button type='submit'><i className='fa fa-search'></i></button>
+      </form>
+
+      {loading ? (
+        <p> Search for Weather Updates </p>
+      ) 
+      : (
+        none ? (
+          <p> Oops.. </p>
+        ) 
+        : (
+          <div className='content' id='content'>
+            <div className='place'>
+              <h1> {weather.name}, {weather.sys?.country} </h1>
+            </div>
+            
+            <div className='temp_detail'>
+              <div className='left'>
+                <img src={`https://openweathermap.org/img/wn/${weather.weather && weather.weather[0].icon}@2x.png`} alt='not available'></img>
+                <p> {weather.weather && weather.weather[0].main} </p>
+              </div>
+              <div className='right'>
+                <div className='temp'>
+                  <h3> Temperature </h3>
+                  <p> {weather.main?.temp} °C </p>
+                </div>
+                <div className='extra'>
+                  <div className='wind'>
+                    <h3> Wind </h3>
+                    <p> {weather.wind?.speed} KM/H </p>
+                  </div>
+                  <div className='humidity'>
+                    <h3> Humidity </h3>
+                    <p> {weather.main?.humidity}% </p>
+                  </div>
+                  <div className='feels'>
+                    <h3> Feels Like </h3>
+                    <p> {weather.main?.feels_like}°C </p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        )
+      )}
+
+      <p id='search_error' style={{display: 'none'}}> Kindly Search the correct place </p>
+      <p id='server_error' style={{display: 'none'}}> Server Error. </p>
     </>
   );
 }
